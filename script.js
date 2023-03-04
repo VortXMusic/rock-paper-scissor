4c7b46afdf2542369d1bc2b495186f38be9aa99d
VortXMusic <katsmusicmadness@gmail.com>
2023-03-04 20:07:00 +0530

    Complete initial variable declarations and computer choice function

---
+++ script.js
@@ -0,0 +1,15 @@
+//Global Variable declaration
+let items = ['Rock','Paper','Scissors'];
+let playerScore = 0, computerScore = 0;
+let endFlag=false;
+
+
+//
+let getComputerChoice = () => items[Math.floor(Math.random()*items.length)];
+
+
+
+do
+{
+
+}while(endFlag==false)
